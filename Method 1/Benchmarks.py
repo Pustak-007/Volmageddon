495a6318110b40ccd532c37daec1c10e3477f439
Poudel Pustak <pustak@Poudels-MacBook-Air.local>
2025-07-23 13:14:40 +0800

    Some minor changes

---
+++ Method 1/Benchmarks.py
@@ -0,0 +1,14 @@
+import Method1_data
+import matplotlib.pyplot as plt
+import yfinance as yf
+import pandas as pd
+from matplotlib.ticker import FixedFormatter, FixedLocator, NullLocator
+import matplotlib.dates as mdates
+SVXY_data = Method1_data.SVXY_data
+initial_capital = 1 #Standard practice to make returns comparative and readable
+equity_curve = pd.DataFrame()
+
+
+
+
+
