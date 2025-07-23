c0c921c91ba76184b459f688b4c9875ac5358a6b
Poudel Pustak <pustak@Poudels-MacBook-Air.local>
2025-07-23 22:12:07 +0800

    Making the code within this module more modular/reusable

--- Method 1/StrategyVsBenchmark.py
+++ Method 1/StrategyVsBenchmark.py
@@ -5,29 +5,22 @@ import yfinance as yf
 import pandas as pd
 from matplotlib.ticker import FixedFormatter, FixedLocator, NullLocator
 import matplotlib.dates as mdates
+from functools import partial
 initial_capital = 1 #Standard practice to make returns comparative and readable
 
 #Creation of SVXY Unit Equity Curve Data
-def Create_UnitDollar_SVXY_Equity_Curve():
+def Create_UnitDollar_Equity_Curve(data):
     equity_curve = pd.DataFrame()
-    equity_curve.index = SVXY_data.index
-    equity_curve['PnL (%)'] = (SVXY_data['Close'].pct_change() * 100).fillna(0)
+    equity_curve.index = data.index
+    equity_curve['PnL (%)'] = (data['Close'].pct_change() * 100).fillna(0)
     equity_curve['Growth Factor'] = 1 + (equity_curve['PnL (%)'])/100
     equity_curve['Equity'] = initial_capital * equity_curve['Growth Factor'].cumprod()
     return equity_curve
 
-#Creation of SPY Unit Equity Cuvre
-def Create_UnitDollar_SPY_Equity_Curve():
-    equity_curve = pd.DataFrame()
-    equity_curve.index = SPY_data.index 
-    equity_curve['PnL (%)'] = (SPY_data['Close'].pct_change() * 100).fillna(0)
-    equity_curve['Growth Factor'] = 1 + (equity_curve['PnL (%)'])/100
-    equity_curve['Equity'] = initial_capital * equity_curve['Growth Factor'].cumprod()
-    return equity_curve    
-
+Create_UnitDollar_SVXY_Equity_Curve = partial(Create_UnitDollar_Equity_Curve, data = SVXY_data)
+Create_UnitDollar_SPY_Equity_Curve = partial(Create_UnitDollar_Equity_Curve, data = SPY_data)
 equity_curve1 = Create_UnitDollar_SVXY_Equity_Curve()
 equity_curve2 = Create_UnitDollar_SPY_Equity_Curve()
-
 
 def Plot_UnitDollar_SVXYvsSPY_Equity_Curve():
     fig, ax = plt.subplots(figsize = (16,8), dpi = 100)
@@ -39,7 +32,7 @@ def Plot_UnitDollar_SVXYvsSPY_Equity_Curve():
     ax.plot(x2,y2, label = "SPY (Benchmark)", color = 'purple')
     ax.grid(True, alpha = 0.3)
     ax.set_xlabel("Date", fontsize = 14)
-    ax.set_ylabel("Value", fontsize = 14)
+    ax.set_ylabel("Equity Value", fontsize = 14)
     ax.set_yscale('log')
     #manual y_ticks
     y_ticks = [1,2,5,10,15,20,25]
