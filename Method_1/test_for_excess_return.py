adf2f6d79b19eeab7eb32be2d074918cf995d46d
Poudel Pustak <pustak@Poudels-MacBook-Air.local>
2025-07-24 15:04:53 +0800

    Testing to see the logic implmentation

---
+++ Method_1/test_for_excess_return.py
@@ -0,0 +1,12 @@
+import numpy as np
+from StrategyVsBenchmark import SVXY_Unit_Equity_Curve
+from USTY3MO import USTY3M0
+excess_return = SVXY_Unit_Equity_Curve['Daily PnL(%)'] - USTY3M0['Rates']
+print(SVXY_Unit_Equity_Curve['Daily PnL(%)'])
+print('-' * 100)
+print(USTY3M0)
+print('-'*100)
+excess_return.iloc[0] = np.nan
+#As we don't have the returns for the very first day - as there is no point of comparision
+# -- so we set the excess return to be NaN for that day
+print(excess_return)
