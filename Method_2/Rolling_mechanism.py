9a15deb3ed6b994d868f1b3e036d72502178d283
Poudel Pustak <pustak@Poudels-MacBook-Air.local>
2025-08-07 21:26:57 +0800

    Made it more modular

--- Method_2/Rolling_mechanism.py
+++ Method_2/Rolling_mechanism.py
@@ -9,7 +9,7 @@ big_dataframe = pd.DataFrame()
 ticker = 'SPY' 
 beginning_date = pd.Timestamp(2012,1,1)
 ending_date = pd.Timestamp(2023,1,10)
-def rolling_mechanism_test_df(ticker = ticker, beginning_date = beginning_date, ending_date = ending_date):
+def rolling_mechanism_df(ticker = ticker, beginning_date = beginning_date, ending_date = ending_date):
     from one_month_test import underlying_closing_price_at_exp_list, underlying_closing_price_at_open_list, call_strike_list, put_strike_list, total_premium_list, total_profit_list, total_payout_list
     from option_chain_func import list_of_relevant_valid_expiration_dates
     distinct_trading_dates_full = pd.to_datetime(pd.read_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/dinstinct_trading_dates.csv')['Dates'])
@@ -22,7 +22,7 @@ def rolling_mechanism_test_df(ticker = ticker, beginning_date = beginning_date, 
     #creation of the big dataframe - the dataframe to be displayed.
     big_dataframe.index = rebalancing_days
     big_dataframe.index.name = 'Open_Date'
-    big_dataframe['Expiration_date'] = list_of_relevant_valid_expiration_dates
+    big_dataframe['Expiration_Date'] = list_of_relevant_valid_expiration_dates
     big_dataframe['Underlying_at_Open'] = underlying_closing_price_at_open_list
     big_dataframe['Underlying_at_Expiraion'] = underlying_closing_price_at_exp_list
     big_dataframe['Call_Strikes'] = call_strike_list
@@ -32,11 +32,13 @@ def rolling_mechanism_test_df(ticker = ticker, beginning_date = beginning_date, 
     big_dataframe['Total PnL'] = total_profit_list
     if len(big_dataframe['Expiration_date']) != len(big_dataframe.index):
         raise ValueError("So, it seems we are not retrieving the updated list as you expected.")
-    return big_dataframe
+    df_to_store = big_dataframe
+    df_to_store.to_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/Final_PnL_DataFrame(Method-2).csv')
+    return df_to_store
     #return converted_list
 #df_to_store = rolling_mechanism_test_df()
 #df_to_store.to_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/Final_PnL_DataFrame_Method_2.csv')
-def rolling_mechanims_test_list(ticker = ticker, beginning_date = beginning_date, ending_date = ending_date):
+def rolling_mechanism_list(ticker = ticker, beginning_date = beginning_date, ending_date = ending_date):
     distinct_trading_dates_full = pd.to_datetime(pd.read_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/dinstinct_trading_dates.csv')['Dates'])
     #distinct_trading_dates = distinct_trading_dates_full[(distinct_trading_dates_full>=beginning_date) & (distinct_trading_dates_full<=ending_date)]
     rebalancing_days_full = pd.to_datetime(pd.read_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/first_trading_day_of_eachMonth.csv')['Dates'])
@@ -45,7 +47,7 @@ def rolling_mechanims_test_list(ticker = ticker, beginning_date = beginning_date
     converted_list = [float(x) for x in profit_list]
     return converted_list
 start_time = time.time()
-print(rolling_mechanism_test_df())
+print(rolling_mechanism_df())
 end_time = time.time()
 #print(rolling_mechanims_test_list())
 print(f"Execution time: {end_time - start_time}")
