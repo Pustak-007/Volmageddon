a9184a424a34cd056dc4c5542f06c0d64d35f83e
Poudel Pustak <pustak@Poudels-MacBook-Air.local>
2025-08-07 21:28:00 +0800

    Minor Changes

--- Method_2/distinct_trading_dates.py
+++ Method_2/distinct_trading_dates.py
@@ -1,7 +1,7 @@
 import wrds 
 import pandas as pd
-beginning_date = pd.Timestamp(2010,11,4)
-end_date = pd.Timestamp(2023,7,29)
+beginning_date = pd.Timestamp(2011,10,4)
+end_date = pd.Timestamp(2023,8,1)
 import wrds 
 db = wrds.Connection()
 
@@ -9,10 +9,10 @@ def give_distinct_trading_dates(start = beginning_date, end = end_date):
     distinct_date_query = f"""select distinct date from crsp.dsf
     where date between '{start}' and '{end}' order by date desc"""
     helper_df = db.raw_sql(distinct_date_query)
+    helper_df.rename(columns = {'date':'Dates'}, inplace=True)
     return helper_df
 if __name__ == "__main__":
     pd.set_option('display.min_rows', 600)
     #print(give_distinct_trading_dates())
     trading_day_series = give_distinct_trading_dates()
     trading_day_series.to_csv('/Users/pustak/Desktop/Volmageddon/Local_Data/dinstinct_trading_dates.csv', index = False)
-    
